6d8661c982597d95911d661df6e607d9b2d38597
gustavomariano12 <gustavo.mariano01@etec.sp.gov.br>
2024-05-29 23:06:13 -0300

    feat: authetication middleware was added

---
+++ app/Http/Middleware/Auth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\Usuario;
+
+class Auth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $req, Closure $next): Response
+    {
+        $token = $req->cookie('Token');
+        if($token == null){
+            return redirect('/');
+        }
+        return $next($req);
+    }
+}
+    
--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,20 @@ Route::post('/', [UsuarioControllers::class, "signIn" ])->middleware(\App\Http\M
 Route::get('/signup', [UsuarioControllers::class, "viewSignUp" ]);
 Route::post('/signup', [UsuarioControllers::class, "signUp"])->middleware(\App\Http\Middleware\ValidationSignUp::class);
 
-Route::get('/account', [UsuarioControllers::class, "getAccount"]);
+Route::get('/account', [UsuarioControllers::class, "getAccount"])->middleware(\App\Http\Middleware\Auth::class);;
 
-Route::get('/updateaccount', [UsuarioControllers::class, "getUpdateAccount"]);
-Route::post('/updateaccount', [UsuarioControllers::class, "updateAccount"])->middleware(\App\Http\Middleware\ValidationUpdateAccount::class);
+Route::get('/updateaccount', [UsuarioControllers::class, "getUpdateAccount"])->middleware(\App\Http\Middleware\Auth::class);;
+Route::post('/updateaccount', [UsuarioControllers::class, "updateAccount"])->middleware(\App\Http\Middleware\Auth::class)->middleware(\App\Http\Middleware\ValidationUpdateAccount::class);
 
 Route::get('/logout', [UsuarioControllers::class, "logout"])->name('logout');
 
 
 
-Route::get('/dashboard', [ClienteController::class, "index" ]);
-Route::get('/cadastrar', [ClienteController::class, "cadastrar" ]);
-Route::post('/adicionar', [ClienteController::class, "adicionar" ]);
-Route::get('/editar/{id}', [ClienteController::class, "editar" ]);
-Route::get('/excluir/{id}', [ClienteController::class, "excluir" ]);
-Route::post('/atualizar/{id}', [ClienteController::class, "atualizar"]);
+Route::get('/dashboard', [ClienteController::class, "index" ])->middleware(\App\Http\Middleware\Auth::class);
+Route::get('/cadastrar', [ClienteController::class, "cadastrar" ])->middleware(\App\Http\Middleware\Auth::class);
+Route::post('/adicionar', [ClienteController::class, "adicionar" ])->middleware(\App\Http\Middleware\Auth::class);
+Route::get('/editar/{id}', [ClienteController::class, "editar" ])->middleware(\App\Http\Middleware\Auth::class);
+Route::get('/excluir/{id}', [ClienteController::class, "excluir" ])->middleware(\App\Http\Middleware\Auth::class);
+Route::post('/atualizar/{id}', [ClienteController::class, "atualizar"])->middleware(\App\Http\Middleware\Auth::class);
 
 
